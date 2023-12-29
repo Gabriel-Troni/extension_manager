9a4d9fc4d8c8a521531f9194efc4a2935f71a029
Gabriel Troni <troni.gabriel.10@gmail.com>
2023-12-29 16:25:38 -0300

    debug

--- api/connection.php
+++ api/connection.php
@@ -1,15 +1,15 @@
 <?php
-$db_ca      = $_ENV['PLANETSCALE_SSL_CERT_PATH'];
+$db_ca      = $_ENV["PLANETSCALE_SSL_CERT_PATH"];
 $server     = $_ENV["PLANETSCALE_DB_HOST"];
-$user       = $_ENV["PLANETSCALE_DB_USERNAME"];
+$userName   = $_ENV["PLANETSCALE_DB_USERNAME"];
 $password   = $_ENV["PLANETSCALE_DB_PASSWORD"];
 $db         = $_ENV["PLANETSCALE_DB"];
 
 function connect(){
-global $db_ca, $server, $user, $password, $db;
+global $db_ca, $server, $userName, $password, $db;
 $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, $db_ca, NULL, NULL);
-mysqli_real_connect($conn, $server, $user, $password, $db, NULL, NULL, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, $server, $userName, $password, $db, NULL, NULL, MYSQLI_CLIENT_SSL);
 
 if(!$conn){
 die("Database connection error: " . mysqli_connect_error());
